88855f81af521e8acc9ca729bd354f4db48b6639
露 万 <960428987@qq.com>
2018-09-28 08:47:46 +0800

    代码

---
+++ Coldairarrow.Fx.Core.Easyui.GitHub-master/README.md
@@ -0,0 +1,4 @@
+# Coldairarrow.Fx.Core.Easyui.GitHub
+Web后台快速开发框架,.NETCore2.1版本
+
+使用方式：https://github.com/Coldairarrow/Coldairarrow.Fx.Core.Easyui.GitHub/wiki
---
+++ Coldairarrow.Fx.Core.Easyui.GitHub-master/src/Coldairarrow.Web/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Coldairarrow.Web
+{
+    [IgnoreLogin]
+    public class TestController : BaseMvcController
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult RequestDemo()
+        {
+            return View();
+        }
+    }
+}
